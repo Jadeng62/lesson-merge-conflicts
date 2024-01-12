e08ae4806906fd11a6120b8fbaefbfce4b54dc37
jadenglenn <jadenglenn@pursuit.org>
2024-01-12 11:47:53 -0500

    updated to otherPerson

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function createPerson(name, age) {
     };
   }
   
-  function addPersonAsFriend(person, other) {
-    person.friends.push(other);
+  function addPersonAsFriend(person, otherPerson) {
+    person.friends.push(otherPerson);
     return person;
   }
   
